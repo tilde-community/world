d94a6314272b301e678e05d53226b83e40527935
Kiel Labian <kiellabian@gmail.com>
2016-03-09 13:35:58 +0800

    refactored Monster object

--- monsters.py
+++ monsters.py
@@ -2,15 +2,30 @@ from world import printer
 
 
 class Monster(object):
-    name = 'Monster'
+
+    def __init__(self, name, level, intro_message, body_message,
+                 defeated_message, attack_message, evaluate_function):
+        self.name = name
+        self.level = level
+        self.intro_message = intro_message
+        self.body_message = body_message
+        self.defeated_message = defeated_message
+        self.attack_message = attack_message
+        self.evaluate_function = evaluate_function
+
+    def introduction(self):
+        printer(self.intro_message)
+        printer(self.body_message)
 
     def attack(self):
-        printer('attack')
+        printer(self.attack_message)
+        printer('You received {0} damage!'.format(self.level))
 
-    def evaluate(ans):
-        pass
+    def defeat(self):
+        printer(self.defeated_message)
 
-
-def create_monster(name, level, intro_message, body_message, defeated_message,
-                   attack_message, evaluate_function):
-    pass
+    def evaluate(self, ans):
+        try:
+            return self.evaluate_function(ans)
+        except:
+            return False
